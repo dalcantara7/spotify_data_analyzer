4bb70fbb5e1107f191430d852399e2db70a046a1
Diego Alcantara <dalcantara7@email.arizona.edu>
2020-05-13 10:53:31 -0700

    updated the script and added requirements

--- spotify_analysis.py
+++ spotify_analysis.py
@@ -5,78 +5,74 @@ import matplotlib
 import numpy as np
 import operator
 import time
-
-data = pd.read_json("Spotify Analysis/one_year_streaming_history0.json", orient='records')
-
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemeber', 'October', 'November', 'December']
+import sys
 
 N = 15
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'Septemeber', 'October', 'November', 'December']
 
-month_dict = {}
-for index, row in data.iterrows():
-    month = str(row['endTime'])[5:7]
-    artist_name = row['artistName']
-    if month not in month_dict:
-        month_dict[month] = {artist_name : row['msPlayed']}
-    else:
-        if artist_name not in month_dict[month]:
-            month_dict[month][artist_name] = row['msPlayed']
-        else:
-            month_dict[month][artist_name] += row['msPlayed']
 
-for i in range(0,12):
-    if i < 9:
-        artists = list(month_dict['0' + str(i+1)].keys())
-        playtimes = list(month_dict['0' + str(i+1)].values())
-    else:
-        artists = list(month_dict[str(i+1)].keys())
-        playtimes = list(month_dict[str(i+1)].values())
+def main(filename):
+    df_data = pd.read_json(filename, orient='records')
 
-    sorted_artists = [x for _, x in sorted(zip(playtimes, artists), reverse=True)]
-    sorted_artists = sorted_artists[0:N]
-    sorted_playtimes = np.array(sorted(playtimes, reverse=True)) / 1000
-    sorted_playtimes = sorted_playtimes[0:N]
+    df_data['endTime'] = pd.to_datetime(df_data['endTime']) 
 
-    cmap = cm.get_cmap('cool', len(sorted_artists))
+    df_by_artist = df_data.groupby([df_data['endTime'].dt.month, df_data['artistName']]).sum().reset_index()
+    df_by_artist.rename(columns={'endTime': 'month'}, inplace=True)
+
+    list_dfs_month = [None] * 12
+
+    for i in range(len(list_dfs_month)):
+        list_dfs_month[i] = df_by_artist[df_by_artist['month'] == i + 1].sort_values(by=['msPlayed'], ascending=False).reset_index()
+        list_dfs_month[i].drop(['index', 'month'], axis=1, inplace=True)
+
+    for month_df in list_dfs_month:
+        month_df['msPlayed'] = month_df['msPlayed'] / 1000
+        month_df.rename(columns={'msPlayed' : 'secPlayed'}, inplace=True)
+
+    cmap = cm.get_cmap('cool', N)
+    colors = []
+    for i in range(cmap.N):
+        colors.append(cmap(i))
+
+    for i in range(12):
+        n = min(list_dfs_month[i].shape[0], N)
+
+        fig, ax = plt.subplots()
+        y_pos = np.arange(n)
+        plt.bar(y_pos, list_dfs_month[i].secPlayed[:n], align='center', alpha=0.9, color=colors)
+        plt.xticks(y_pos, list_dfs_month[i].artistName[:n], rotation = 85)
+        formatter = matplotlib.ticker.FuncFormatter(lambda sec, y: time.strftime('%H:%M:%S', time.gmtime(sec)))
+        ax.yaxis.set_major_formatter(formatter)
+        plt.gcf().subplots_adjust(bottom=0.4, left=0.2)
+        plt.title("Top " + str(n) + " Spotify Artist Listening Time " + months[i] + " 2019")
+        plt.ylabel('Listening Time')
+
+        plt.show()
+
+
+    top_artist_by_month = []
+    time_played_by_month = []
+    for i in range(len(list_dfs_month)):
+        top_artist_by_month.append(list_dfs_month[i].iloc[0].artistName + " (" + str(months[i][0:3]) + ")")
+        time_played_by_month.append(list_dfs_month[i].iloc[0].secPlayed)
+
+    cmap = cm.get_cmap('cool', 12)
     colors = []
     for j in range(cmap.N):
         colors.append(cmap(j))
 
     fig, ax = plt.subplots()
-    y_pos = np.arange(len(sorted_artists))
-    plt.bar(y_pos, sorted_playtimes, align='center', alpha=0.9, color=colors)
-    plt.xticks(y_pos, sorted_artists, rotation = 85)
+    y_pos = np.arange(12)
+    plt.bar(y_pos, time_played_by_month, align='center', alpha=0.9, color=colors)
+    plt.xticks(y_pos, top_artist_by_month, rotation = 85)
     formatter = matplotlib.ticker.FuncFormatter(lambda sec, y: time.strftime('%H:%M:%S', time.gmtime(sec)))
     ax.yaxis.set_major_formatter(formatter)
-    plt.gcf().subplots_adjust(bottom=0.4, left=0.2)
-    plt.title("Top " + str(N) + " Spotify Artist Listening Time " + months[i] + " 2019")
+    plt.gcf().subplots_adjust(bottom=0.5, left=0.2)
+    plt.title("Top Spotify Artist Of Each Month")
     plt.ylabel('Listening Time')
 
     plt.show()
 
-max_artist_per_month = []
-values = []
-i = 0
-for month in month_dict.keys():
-    max_artist_per_month.append(str(max(month_dict[month].items(), key=operator.itemgetter(1))[0]) + " (" + months[i][0:3] + ")")
-    values.append(max(month_dict[month].values()))
-    i+=1
 
-values = np.array(values) / 1000
-
-cmap = cm.get_cmap('cool', 12)
-colors = []
-for j in range(cmap.N):
-    colors.append(cmap(j))
-
-fig, ax = plt.subplots()
-y_pos = np.arange(12)
-plt.bar(y_pos, values, align='center', alpha=0.9, color=colors)
-plt.xticks(y_pos, max_artist_per_month, rotation = 85)
-formatter = matplotlib.ticker.FuncFormatter(lambda sec, y: time.strftime('%H:%M:%S', time.gmtime(sec)))
-ax.yaxis.set_major_formatter(formatter)
-plt.gcf().subplots_adjust(bottom=0.5, left=0.2)
-plt.title("Top Spotify Artist Of Each Month")
-plt.ylabel('Listening Time')
-
-plt.show()
+if __name__ == "__main__":
+    main(sys.argv[1])
